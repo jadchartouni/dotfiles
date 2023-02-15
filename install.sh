215ae9d30726dfe5e04c06c9e54c8950a8a2e951
Jad Chartouni <jad.chartouni@outlook.com>
2023-02-15 12:28:37 +0400

    Revert "Removed readme from zsh"
    This reverts commit 39a84599e25d105160301f7bfd1edd140c3ccbf6.

--- install.sh
+++ install.sh
@@ -36,10 +36,6 @@ function setup_tmux()
 }
 
 # Setup ZSH
-#
-# https://github.com/zsh-users/zsh-autosuggestions
-# https://github.com/zsh-users/zsh-syntax-highlighting
-# https://github.com/marlonrichert/zsh-autocomplete
 function setup_zsh()
 {
     echo "Configuring ZSH"
