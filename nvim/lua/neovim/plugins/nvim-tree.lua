aa21f86902be607fe2200615e586b659d8b41c93
Jad Chartouni <jad.chartouni@outlook.com>
2023-09-11 14:23:14 +0400

    Fixed updated in nvim-tree

--- nvim/lua/neovim/plugins/nvim-tree.lua
+++ nvim/lua/neovim/plugins/nvim-tree.lua
@@ -1,25 +1,24 @@
 require("nvim-tree").setup({
-    update_focused_file = {
-        enable = true,
-    },
-    view = {
-        width = 35,
-    },
-    git = {
-        ignore = false,
-    },
-    renderer = {
-        highlight_opened_files = "1",
-        group_empty = true,
-        icons = {
-            show = {
-                folder_arrow = false,
-            },
-        },
-        indent_markers = {
-            enable = true,
-        },
-    },
+	update_focused_file = {
+		enable = true,
+	},
+	view = {
+		width = 35,
+	},
+	git = {
+		ignore = false,
+	},
+	renderer = {
+		group_empty = true,
+		icons = {
+			show = {
+				folder_arrow = false,
+			},
+		},
+		indent_markers = {
+			enable = true,
+		},
+	},
 })
 
 vim.cmd([[
